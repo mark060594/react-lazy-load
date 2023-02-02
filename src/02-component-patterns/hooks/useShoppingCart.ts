d13b11367d15bf1fc49f641bd235f81a2d604e46
Marco <marco@shibena.com>
2023-02-01 20:28:24 -0600

    control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -19,25 +19,7 @@ export const useShoppingCart = () => {
       }) => {
         setShoppingCart((oldShoppingCart) => {
     
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-    
-            if(Math.max(productInCart.count + count, 0) > 0) {
-              productInCart.count += count;
-    
-              return {
-                ...oldShoppingCart,
-                [product.id]: productInCart
-              }
-            }
-    
-            //Delete product
-    
-              const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-              return rest;
-        
-      
-          // Easy way to solve but not the better
-         /*  if (count === 0) {
+          if (count === 0) {
             const { [product.id]: toDelete, ...rest } = oldShoppingCart;
     
             return rest;
@@ -47,7 +29,7 @@ export const useShoppingCart = () => {
             ...oldShoppingCart,
     
             [product.id]: { ...product, count },
-          }; */
+          };
         });
       };
     
